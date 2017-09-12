83710fe8b8eb012bf38deaf97c6e1670abe7c2d5
Younggil Lee <leyoungg@sheridancollege.ca>
2017-09-12 15:12:19 -0400

    Handle action by using middle ware ( redux-promise)

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,9 +1,11 @@
-// import { FETCH_WEATHER } from "../actions/index";
+import { FETCH_WEATHER } from "../actions/index";
 
-// export default function(state = [], action) {
-//   switch (action.type) {
-//     case FETCH_WEATHER:
-//       return [action.payload.data, ...state];
-//   }
-//   return state;
-// }
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [action.payload.data, ...state];
+      //return state.concat([action.payload.data]);
+      
+  }
+  return state;
+}
